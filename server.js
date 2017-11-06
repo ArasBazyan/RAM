a17baaae3336f9293648161f82599a7e8a10cf19
Aras Bazyan <aras.bazyan@gmail.com>
2017-11-06 16:46:15 +0100

    Push to pull

--- server.js
+++ server.js
@@ -1,24 +1,71 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
+var sqlite3 = require("sqlite3").verbose();
 
+
+var db = new sqlite3.Database('Volvo.db');
+
+
+
+var rowid;
+
+
+
+db.serialize(function() {
+
+
+    db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
+        rowid = row.id;
+        console.log(row.id + ": " + row.info + " ----- "  + rowid);
+    });
+});
+
+
+db.close();
+
+
+var loadID = function (req, res, next) {
+        var id = req.params.id;
+        var info = rowid;
+        if(info){
+            req.info = info;
+            next();
+        } else {
+            next(new Error("Failed to load " + id));
+
+        }
+
+
+}
 
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 
 app.get('/',function(req,res){
-    res.sendfile(path.join(__dirname+'/adminview.html'));
+    res.sendFile(path.join(__dirname+'/adminview.html'));
 });
 
 app.get('/adminView',function(req,res){
     res.sendFile(path.join(__dirname+'/adminview.html'));
+
 });
 
-app.get('/projectDetail',function(req,res){
+app.get('/projectDetail/:id', loadID, function (req, res) {
+    res.send('Seeing user ' + req.info);
+    
+})
+
+/*
+app.get('/projectDetail/:id',function(req,res){
     res.sendFile(path.join(__dirname+'/projectDetail.html'));
-});
+    var id = req.params.id;
+    var query = "select * from user_info where ID = " + id;
 
+    console.log(req.params.id + " ---- " + query);
+});
+*/
 app.get('/nodeAdmin',function(req,res){
     res.sendFile(path.join(__dirname+'/nodeAdmin.html'));
 });
