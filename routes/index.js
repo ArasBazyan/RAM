6af1a954d3b2f791f64ce6f2417c07f15e88a7ee
Mayra Soliz <mayrageraldine@icloud.com>
2017-12-06 20:45:31 +0100

    fixing error

--- routes/index.js
+++ routes/index.js
@@ -260,7 +260,7 @@ router.post('/createRoot', function(req, res){
     //res.send("data")
     var db = new sqlite3.Database('./Volvo.db');
 
-    db.run(`INSERT INTO Organization (OrganizationName, idParentOrganization)
+    db.run(`INSERT INTO Organization (dOrganizationName, idParentOrganization)
             VALUES(?,?)`, [rName, rDescription], function(err) {
         if (err){
             console.log("error:", rName + " " +rDescription );
